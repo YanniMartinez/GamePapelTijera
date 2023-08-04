66773a1968b36e17b97febf345344a43d240340b
YanniMartinez <metalyanni@gmail.com>
2023-08-04 15:46:59 -0600

    Initial Commit

---
+++ GamePapelTijera/GamePapelTijeraApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamePapelTijeraApp.swift
+//  GamePapelTijera
+//
+//  Created by MacBook 26 on 04/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GamePapelTijeraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
