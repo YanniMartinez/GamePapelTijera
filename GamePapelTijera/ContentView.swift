b7a1e45daa9b1e2681b19f220c48b5bf6e673574
YanniMartinez <metalyanni@gmail.com>
2023-08-04 16:19:56 -0600

    Add principal text

--- GamePapelTijera/ContentView.swift
+++ GamePapelTijera/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
         ZStack {
             BackgroundColor()
+            
+            VStack{
+                
+                Text("Steps: ")
+                    .font(.title)
+                Text("Your Score is: ")
+                    .font(.title)
+                
+                Text("Select your option")
+                    .font(.largeTitle)
+                    .padding(.top,100)
+            }
         }
         
     }
